bed7abab96f90c51c799273fd70daa13d4d88caa
Mason <mariofan717@gmail.com>
2024-02-01 01:25:52 -0800

    Project 0 completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     
@@ -14,6 +19,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent <Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
     
     private void FixedUpdate() 
@@ -34,6 +42,18 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp")) 
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    
+    void SetCountText() 
+    {
+        countText.text = "Count: " + count.ToString();
+        
+        if (count >= 8)
+        {
+            winTextObject.SetActive(true);
         }
     }
 }
